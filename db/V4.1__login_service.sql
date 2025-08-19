e2399cc8360a0ee7dcb645a5b775eb3a53fa531e
Tobias Peters <tobias.peters@kreativeffekt.at>
2025-08-19 15:55:03 +0200

    remove unused columns

--- db/V4.1__login_service.sql
+++ db/V4.1__login_service.sql
@@ -27,55 +27,8 @@ CREATE TABLE IF NOT EXISTS user
     ShardId               int(10)                                            NOT NULL DEFAULT '-1',
     State                 enum ('Offline','Online', 'Authorized', 'Waiting') NOT NULL DEFAULT 'Offline',
     Privilege             varchar(255)                                       NOT NULL DEFAULT '',
-/*
-    GroupName             varchar(255)                                       NOT NULL DEFAULT '',
-    FirstName             varchar(255)                                       NOT NULL DEFAULT '',
-    LastName              varchar(255)                                       NOT NULL DEFAULT '',
-    Birthday              varchar(32)                                        NOT NULL DEFAULT '',
-    Gender                tinyint(1) unsigned                                NOT NULL DEFAULT '0',
-    Country               char(2)                                            NOT NULL DEFAULT '',
-    Email                 varchar(255) unique                                NOT NULL DEFAULT '',
-    Address               varchar(255)                                       NOT NULL DEFAULT '',
-    City                  varchar(100)                                       NOT NULL DEFAULT '',
-    PostalCode            varchar(10)                                        NOT NULL DEFAULT '',
-    USState               char(2)                                            NOT NULL DEFAULT '',
-    Chat                  char(2)                                            NOT NULL DEFAULT '0',
-    BetaKeyId             int(10) unsigned                                   NOT NULL DEFAULT '0',
-    CachedCoupons         varchar(255)                                       NOT NULL DEFAULT '',
-    ProfileAccess         varchar(45)                                                 DEFAULT NULL,
-    Level                 int(2)                                             NOT NULL DEFAULT '0',
-    CurrentFunds          int(4)                                             NOT NULL DEFAULT '0',
-    IdBilling             varchar(255)                                       NOT NULL DEFAULT '',
-    Community             char(2)                                            NOT NULL DEFAULT '--',
-    Newsletter            tinyint(1)                                         NOT NULL DEFAULT '1',
-    Account               varchar(64)                                        NOT NULL DEFAULT '',
-    ChoiceSubLength       tinyint(2)                                         NOT NULL DEFAULT '0',
-    CurrentSubLength      varchar(255)                                       NOT NULL DEFAULT '0',
-    ValidIdBilling        int(4)                                             NOT NULL DEFAULT '0',
-    GMId                  int(4)                                             NOT NULL DEFAULT '0',
-*/
     ExtendedPrivilege     varchar(128)                                       NOT NULL DEFAULT '',
-/*
-    ToolsGroup            varchar(20)                                        NOT NULL DEFAULT '',
-    Unsubscribe           date                                               NOT NULL DEFAULT '0000-00-00',
-    SubDate               datetime                                           NOT NULL DEFAULT '0000-00-00 00:00:00',
-    SubIp                 varchar(20)                                        NOT NULL DEFAULT '',
-    SecurePassword        varchar(32)                                        NOT NULL DEFAULT '',
-    LastInvoiceEmailCheck date                                               NOT NULL DEFAULT '0000-00-00',
-    FromSource            varchar(8)                                         NOT NULL DEFAULT '',
-    ValidMerchantCode     varchar(13)                                        NOT NULL DEFAULT '',
-    PBC                   tinyint(1)                                         NOT NULL DEFAULT '0',
-    ApiKeySeed            varchar(8)                                                  DEFAULT NULL,
- */
     Cookie                VARCHAR(30)                                        NOT NULL DEFAULT ''
-/*
-    ,
-    index GroupIndex (GroupName),
-    index ToolsGroup (ToolsGroup),
-    index CurrentSubLength (CurrentSubLength),
-    index Community (Community),
-    index GMId (GMId)
-*/
 ) COMMENT ='contains all users information for login system';
 
 -- add default user with login 'snowballs' and password 'snowballs
